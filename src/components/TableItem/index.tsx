33790c2719f8da3b418ca09de56b575d35104750
Thiago Ferreira <thi.ferreira.silva03@gmail.com>
2022-09-05 20:23:53 -0300

    full item display

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,16 +1,26 @@
 import * as SC from "./styles"
 import { Item } from "../../types/Item"
+import { categories } from "../../data/categories"
 import { formatCurrency } from "../../utils/formatCurrency"
+import { formatDate } from "../../helpers/dateFilter"
 interface TableItemProps {
     item: Item
 }
-export const TableItem = ({ item }:TableItemProps) => {
-    return(
+export const TableItem = ({ item }: TableItemProps) => {
+    return (
         <SC.TableLine>
-            <SC.TableColumn></SC.TableColumn>
-            <SC.TableColumn>{item.category}</SC.TableColumn>
+            <SC.TableColumn>{formatDate(item.date)}</SC.TableColumn>
+            <SC.TableColumn>
+                <SC.Category color={categories[item.category].color}>
+                    {categories[item.category].title}
+                </SC.Category>
+            </SC.TableColumn>
             <SC.TableColumn>{item.title}</SC.TableColumn>
-            <SC.TableColumn>{formatCurrency(item.value)}</SC.TableColumn>
+            <SC.TableColumn >
+                <SC.Value 
+                color={categories[item.category].expense ? 'red' : 'green'}>{formatCurrency(item.value)}
+            </SC.Value>
+            </SC.TableColumn>
         </SC.TableLine>
     )
 }
--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,6 +1,6 @@
 import { Category } from "../types/Category"
 export const categories: Category = {
     food: { title: 'Alimentation', color: 'blue', expense: true },
-    rent: { title: 'rent', color: 'brown', expense: true },
+    rent: { title: 'Rent', color: 'brown', expense: true },
     salary: { title: 'Salary', color: 'gray', expense: false}
 }
